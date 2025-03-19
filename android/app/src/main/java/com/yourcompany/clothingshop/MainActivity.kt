662f0fa84c0cedf43d8a25b17efacb5b96225200
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2025-03-19 12:16:26 +0700

    oauth

--- android/app/src/main/java/com/yourcompany/clothingshop/MainActivity.kt
+++ android/app/src/main/java/com/yourcompany/clothingshop/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nhclothing.app
+package com.yourcompany.clothingshop
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/yourcompany/clothingshop/MainApplication.kt
+++ android/app/src/main/java/com/yourcompany/clothingshop/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.nhclothing.app
+package com.yourcompany.clothingshop
 
 import android.app.Application
 import android.content.res.Configuration
